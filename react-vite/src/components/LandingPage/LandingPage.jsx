4cd3a448efc85412989f0a9bc5019a4193d39fdf
Jasmine <jasminetruong97@gmail.com>
2024-03-28 10:19:49 -0700

    landing/details styling

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getAllProductsThunk } from "../../redux/product"
 import { NavLink } from 'react-router-dom';
+import './LandingPage.css'
 
 function LandingPage(){
     const dispatch = useDispatch()
@@ -15,15 +16,39 @@ function LandingPage(){
         return <div>Loading...</div>
     }
 
+    function formatDescription(description){
+        let newDescription = description.slice(0,100) + '...'
+        return newDescription
+    }
+
+    function reviewLength(){
+
+    }
+    function avgRating(){
+
+    }
+
+
     return(
-        <>
+        <div className='landing-page-container'>
             {productsArr?.map (product => (
-                <NavLink className='product-container' to={`/products/${product.id}`} key={product.id}>
-                    <img src={product.image_url}/>
-                    <div>{product.name}</div>
-                </NavLink>
+                <div className='landing-product-container'>
+                    <NavLink className='landing-nav-container' to={`/products/${product.id}`} key={product.id}>
+                        <img src={product.image_url} className='product-img'/>
+                        <div className='product-info'>
+                            <div>{product.name}</div>
+                            <div>{formatDescription(product.description)}</div>
+                            <div className='landing-rating-container'>
+                                <div>Product Rating here</div>
+                                <div>Number of Ratings here</div>
+                            </div>
+                            <div>${product.price}</div>
+                            <button>Add to cart</button>
+                        </div>
+                    </NavLink>
+                </div>
             ))}
-        </>
+        </div>
     )
 }
 
--- react-vite/src/components/ProductDetails/ProductDetails.jsx
+++ react-vite/src/components/ProductDetails/ProductDetails.jsx
@@ -7,6 +7,7 @@ import { reviewsByProductThunk } from "../../redux/review";
 import { getAllUsersThunk } from "../../redux/users";
 import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
 import DeleteReview from "../DeleteReview/DeleteReview";
+import './ProductDetails.css'
 
 function ProductDetails(){
     const dispatch = useDispatch()
@@ -44,20 +45,25 @@ function ProductDetails(){
     }
 
     return(
-        <>
+        <div className='details-page'>
+        <div className='product-details-page'>
             <div className='product-detail-container'>
-                <img src={oneProduct.image_url}/>
+                <img src={oneProduct.image_url} className='product-detail-img'/>
                 <div className='product-info-container'>
                     <h1>{oneProduct.name}</h1>
+                    <div className='product-rating-container'>
+                        <div>Product Rating here</div>
+                        <div>Product Num Reviews here</div>
+                    </div>
                     <p>${oneProduct.price}</p>
                     <button><NavLink>Add to Cart</NavLink></button>
                     <h2>About this item</h2>
                     <p>{oneProduct.description}</p>
                     <h2>Seller Information</h2>
                     <div className='seller-details-container'>
-                        <p>Username: {seller?.username}</p>
-                        <p>{seller?.first_name}</p>
-                        <p>Joined: {refactorDate(seller?.createdAt)}</p>
+                        <p className='seller-info-text'>Username: {seller?.username}</p>
+                        <p className='seller-info-text'>{seller?.first_name}</p>
+                        <p className='seller-info-text'>Joined: {refactorDate(seller?.createdAt)}</p>
                     </div>
                 </div>
             </div>
@@ -65,29 +71,32 @@ function ProductDetails(){
                 <h1>Customer Reviews</h1>
                 <button><NavLink to={`/products/${productId}/review/new`}>Write a customer review</NavLink></button>
                 {reviewsArr?.map (review => (
-                    <div key={review?.id}>
+                    <div key={review?.id} className='review-container'>
+                        <div className='review-info-container'>
+                            <p>{users[(review.user_id)-1]?.first_name}</p>
+                            <p>Rating: {review?.rating}</p>
+                            <p>Reviewed on {refactorDate(review.createdAt)}</p>
+                            <p>{review?.review}</p>
+                        </div>
+                        <img src={review?.image_url} className='review-image'/>
                         {review.user_id == currUser.id  && (
                             <>
-                                <button>
+                                <button className='review-btns'>
                                     <OpenModalMenuItem
                                         itemText='Delete Review'
                                         modalComponent={<DeleteReview reviewId={review.id} productId={productId} renderDelete={renderDelete}/>}
                                     />
                                 </button>
-                                <button>
+                                <button className='review-btns'>
                                     <NavLink to={`/products/${productId}/review/${review.id}/edit`}>Update Review</NavLink>
                                 </button>
                             </>
                         )}
-                        <p>{users[(review.user_id)-1]?.first_name}</p>
-                        <p>{refactorDate(review.createdAt)}</p>
-                        <p>Rating: {review?.rating}</p>
-                        <p>{review?.review}</p>
-                        <img src={review?.image_url}/>
                     </div>
                 ))}
             </div>
-        </>
+        </div>
+        </div>
     )
 }
 
